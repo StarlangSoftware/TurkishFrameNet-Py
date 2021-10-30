ca7e71ce1345f98a905a96edb3d4db8adeafa416
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-10-30 19:37:05 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='NlpToolkit-FrameNet',
-    version='1.0.4',
+    version='1.0.5',
     packages=['FrameNet'],
     url='https://github.com/StarlangSoftware/TurkishFrameNet-Py',
     license='',
     author='olcaytaner',
     author_email='olcay.yildiz@ozyegin.edu.tr',
-    description='FrameNet library'
+    description='FrameNet library',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )

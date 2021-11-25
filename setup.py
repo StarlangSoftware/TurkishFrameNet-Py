dc0024653845ed7e692b3fff3c6a5e11c26b95b3
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-11-25 16:13:44 +0300

    Updated setup.py to include resource files in the distribution.

--- setup.py
+++ setup.py
@@ -7,14 +7,13 @@ long_description = (this_directory / "README.md").read_text()
 setup(
     name='NlpToolkit-FrameNet',
     version='1.0.6',
-    packages=['FrameNet'],
+    packages=['FrameNet', 'FrameNet.data'],
+    package_data={'FrameNet.data': ['*.xml']},
     url='https://github.com/StarlangSoftware/TurkishFrameNet-Py',
     license='',
     author='olcaytaner',
     author_email='olcay.yildiz@ozyegin.edu.tr',
     description='FrameNet library',
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    include_package_data=True,
-    package_data={'': ['data/*.xml']},
+    long_description_content_type='text/markdown'
 )

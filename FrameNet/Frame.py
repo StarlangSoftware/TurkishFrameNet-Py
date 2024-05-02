9e436cbc2a8f1f981b59c35954a973ca09fc700c
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2024-05-02 15:00:03 +0300

    Updated the documentation.

--- FrameNet/Frame.py
+++ FrameNet/Frame.py
@@ -4,32 +4,71 @@ class Frame:
     __frame_elements: list
 
     def __init__(self, name: str):
+        """
+        Constructor of Frame class which takes inputStream as input and reads the frame
+        :param name: Name of the frame
+        """
         self.__lexical_units = []
         self.__frame_elements = []
         self.__name = name
 
     def addLexicalUnit(self, lexicalUnit: str):
+        """
+        Adds a new lexical unit to the current frame
+        :param lexicalUnit: lexicalUnit Lexical unit to be added
+        """
         self.__lexical_units.append(lexicalUnit)
 
     def addFrameElement(self, frameElement: str):
+        """
+        Adds a new frame element to the current frame
+        :param frameElement: Frame element to be added
+        """
         self.__frame_elements.append(frameElement)
 
     def lexicalUnitExists(self, synSetId: str) -> bool:
+        """
+        Checks if the given lexical unit exists in the current frame
+        :param synSetId: Lexical unit to be searched.
+        :return: True if the lexical unit exists, false otherwise.
+        """
         return synSetId in self.__lexical_units
 
     def getLexicalUnit(self, index: int) -> str:
+        """
+        Accessor for a given index in the lexicalUnit array.
+        :param index: Index of the lexical unit
+        :return: The lexical unit at position index in the lexicalUnit array
+        """
         return self.__lexical_units[index]
 
     def getFrameElement(self, index: int) -> str:
+        """
+        Accessor for a given index in the frameElements array.
+        :param index: Index of the frame element
+        :return: The frame element at position index in the frameElements array
+        """
         return self.__frame_elements[index]
 
     def lexicalUnitSize(self) -> int:
+        """
+        Returns number of lexical units in the current frame
+        :return: Number of lexical units in the current frame
+        """
         return len(self.__lexical_units)
 
     def frameElementSize(self) -> int:
+        """
+        Returns number of frame elements in the current frame
+        :return: Number of frame elements in the current frame
+        """
         return len(self.__frame_elements)
 
     def getName(self) -> str:
+        """
+        Accessor for the name of the frame
+        :return: Name of the frame
+        """
         return self.__name
 
     def __repr__(self):
--- FrameNet/FrameNet.py
+++ FrameNet/FrameNet.py
@@ -10,6 +10,10 @@ class FrameNet:
     __frames: list
 
     def __init__(self):
+        """
+        A constructor of FrameNet class which reads all frame files inside the files2.txt file. For each filename inside
+        that file, the constructor creates a FrameNet.Frame and puts in inside the frames array.
+        """
         self.__frames = []
         root = xml.etree.ElementTree.parse(pkg_resources.resource_filename(__name__, 'data/framenet.xml')).getroot()
         for frame_node in root:
@@ -24,12 +28,22 @@ class FrameNet:
             self.__frames.append(frame)
 
     def lexicalUnitExists(self, synSetId: str) -> bool:
+        """
+        Checks if the given lexical unit exists in any frame in the frame set.
+        :param synSetId: Id of the lexical unit
+        :return: True if any frame contains the given lexical unit, false otherwise.
+        """
         for frame in self.__frames:
             if frame.lexicalUnitExists(synSetId):
                 return True
         return False
 
     def getFrames(self, synSetId: str) -> list:
+        """
+        Returns an array of frames that contain the given lexical unit in their lexical units
+        :param synSetId: Id of the lexical unit.
+        :return: An array of frames that contains the given lexical unit.
+        """
         result = []
         for frame in self.__frames:
             if frame.lexicalUnitExists(synSetId):
@@ -37,7 +51,16 @@ class FrameNet:
         return result
 
     def size(self) -> int:
+        """
+        Returns number of frames in the frame set.
+        :return: Number of frames in the frame set.
+        """
         return len(self.__frames)
 
     def getFrame(self, index: int) -> Frame:
+        """
+        Returns the element at the specified position in the frame list.
+        :param index: index of the element to return
+        :return: The element at the specified position in the frame list.
+        """
         return self.__frames[index]
